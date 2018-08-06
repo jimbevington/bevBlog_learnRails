a3bcd95bcd85c93d370c11f08ad0bc9c501364ab
Jim Bevington <jim.bevington@hotmail.com>
2018-08-06 18:10:42 +0100

    added a basic navbar partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def article_preview(article)
+    @article = Article.find(article.id)
+    truncate(@article.text, length: 100, separator: ' ')
+  end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,3 @@
 module WelcomeHelper
-  def article_preview(id)
-    @article = Article.find(id)
-    truncate(@article.text, length: 100, separator: ' ')
-  end
+  
 end

7db39c43f487c3950736c23cc86d8c7bdf1269c0
Jim Bevington <jim.bevington@hotmail.com>
2018-08-06 17:01:36 +0100

    added preview to homepage

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,6 @@
 module WelcomeHelper
+  def article_preview(id)
+    @article = Article.find(id)
+    truncate(@article.text, length: 100, separator: ' ')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_143010) do
+ActiveRecord::Schema.define(version: 2018_08_06_152243) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_08_06_143010) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.string "name"
     t.text "body"
     t.integer "article_id"
     t.datetime "created_at", null: false
